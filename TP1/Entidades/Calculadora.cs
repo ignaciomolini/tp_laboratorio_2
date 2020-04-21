8462f613ade79e7b22401ca546949991dd6b180e
Ignacio Molini <ignacio_molini@hotmail.com>
2020-04-21 17:46:22 -0300

    TP subido con la solucion

--- TP1/Entidades/Calculadora.cs
+++ TP1/Entidades/Calculadora.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,65 +58,3 @@ namespace Entidades
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entidades
-{
-    public static class Calculadora
-    {
-        private static string ValidarOperador(string operador)
-        {
-            string retorno = "+";
-
-            switch (operador)
-            {
-                case "-":
-                    retorno = "-";
-                    break;
-
-                case "*":
-                    retorno = "*";
-                    break;
-
-                case "/":
-                    retorno = "/";
-                    break;
-            }
-
-            return retorno;
-        }
-
-        public static double Operar(Numero num1, Numero num2, string operador)
-        {
-            double retorno = 0;
-            string auxOperador = ValidarOperador(operador);
-
-            switch (auxOperador)
-            {
-                case "+":
-                    retorno = num1 + num2;
-                    break;
-
-                case "-":
-                    retorno = num1 - num2;
-                    break;
-
-                case "*":
-                    retorno = num1 * num2;
-                    break;
-
-                case "/":
-                    retorno = num1 / num2;
-                    break;
-            }
-
-            return retorno;
-        }
-    }
-}
->>>>>>> bb146588b1694d59d2be6b78d0b440991fcd537d
